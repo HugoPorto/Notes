b7d34dff971202e1e290b2a4299695ac793c9a74
HugoPorto <victor.almeida.porto7@gmail.com>
2023-04-19 15:30:42 -0300

    Adicionar arquivos de projeto.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://learn.microsoft.com/pt-br/dotnet/maui/tutorials/notes-app/?source=recommendations&view=net-maui-7.0&tutorial-step=4";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+}
